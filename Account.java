0795ba4c1c05411c986d813818cee4085eaf364b
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2023-02-22 16:58:27 +0000

    added the random function kind of

--- Account.java
+++ Account.java
@@ -10,6 +10,18 @@ public class Account {
     // Static Attributes
     public ArrayList<Account> accountList = new ArrayList<>();
 
+    public int gen() {
+      //this bit for if this is the first run
+      Random r = new Random( System.currentTimeMillis() );
+      double randomNumber = ((1 + r.nextInt(9)) * 10000 + r.nextInt(10000));
+      for (int n= 1; randomNumber == ; i++){
+        Random r = new Random( System.currentTimeMillis() );
+        randomNumber = ((1 + r.nextInt(9)) * 10000 + r.nextInt(10000)); 
+      }
+      return randomNumber;
+
+  }
+
 
     //Getter Methods
     public int getAccountId(){
