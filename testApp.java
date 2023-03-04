326ffcea11e1c2973fa5b75bd3f74c8fc6419383
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2023-03-04 13:57:58 +0000

    Add files via upload

---
+++ testApp.java
@@ -0,0 +1,9 @@
+package socialmedia;
+public class testApp {
+    public static void main(String [] args) {
+        for (Object value :accountList.values()){
+            System.out.println(value);
+
+    }
+}
+}
