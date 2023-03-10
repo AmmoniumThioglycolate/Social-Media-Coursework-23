4367695eb0839813abb54763a103e6a8705189c6
Alex-Pryce <101018262+Alex-Pryce@users.noreply.github.com>
2023-03-10 13:49:48 +0000

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,38 +1,68 @@
-
 package socialmedia;
+import java.io.*;
+import java.util.*;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.ArrayList; // Importing the ArrayList class
 
-
-public class Account {
 
     // Instance Attributes
-    public int AccountId;
+    public String AccountId;
     public String Handle;
     public String Description;
 
-    // Static Attributes
-    public Dictionary<String, String> accountDictionary = new Hashtable<String, String>(); //This dictionary allows us to find an AccountID from a handle.
 
-    public static int generateRandomNumber() {
-      //this bit for if this is the first run
-      Random r = new Random( System.currentTimeMillis() );
-      double randomNumber = ((1 + r.nextInt(9)) * 10000 + r.nextInt(10000));
-      /* 
-      for (int n= 1; randomNumber ==  ; i++){
-        Random r = new Random( System.currentTimeMillis() );
-        randomNumber = ((1 + r.nextInt(9)) * 10000 + r.nextInt(10000)); 
-      } */
-      return randomNumber;
+    //Static Attribute - An ArrayList to store the system's Accounts
+    public static ArrayList<Account> accountArrayList = new ArrayList<Account>();
 
-  }
+
+    //Static Methods
+    public static int generateUniqueRandomNumber() {
+          //this bit for if this is the first run
+          Random r = new Random( System.currentTimeMillis() );
+          double randomNumber = ((1 + r.nextInt(9)) * 10000 + r.nextInt(10000));
+          /*
+          for (int n= 1; randomNumber ==  ; i++){
+            Random r = new Random( System.currentTimeMillis() );
+            randomNumber = ((1 + r.nextInt(9)) * 10000 + r.nextInt(10000));
+          } */
+          return Integer.toString(randomNumber);
+
+      }
+
+
+    public static boolean doesItContainWhiteSpaceOrIsEmpty(String handle) {
+      for (int i = 0; i < handle.length(); i++) {
+        if ((handle.charAt(i)).equals(" ")) {
+          throw new InvalidHandleException("blah blah to be filled in later");
+          return true;
+        }
+      }
+      if (handle.equals("")) {
+        throw new InvalidHandleException("blah blah to be filled in later");
+        return true;
+      }
+      return false;
+    }
+
+
     //Getter Methods
-    public int getAccountId() {
+    public String getAccountId() {
       return AccountId;
     }
 
+    public String getHandle() {
+      return handle;
+    }
+    
+
     //Setter Methods
-    public void setAccountId(int newAccountId) {
+    public void setAccountId(String newAccountId) {
       this.AccountId = newAccountId;
     }
+
+    public void setHandle(String newHandle) {
+      this.Handle = newHandle;
+    }
+
 }
