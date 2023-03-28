da70f4fb68aa115c8d2b869548c43843bc7207e7
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2023-03-28 14:49:03 +0000

    added some java docs

--- src/socialmedia/Account.java
+++ src/socialmedia/Account.java
@@ -1,9 +1,17 @@
+// A package declaration. It is used to group classes together.
 package socialmedia;
-
 import java.util.ArrayList;
 import java.util.Random;
 
+
+/**
+ * The Account class is a class that stores the account's ID, handle and description. It also has
+ * static methods that generate a unique random number, check if the account ID is recognised, check if
+ * the handle exists, check if the handle contains white space or is empty, check if the handle is
+ * invalid, and getter and setter methods.
+ */
 public class Account {
+
 
     // Instance Attributes
     public int accountId;
@@ -17,6 +25,14 @@ public class Account {
 
 
     //Static Methods
+
+    /**
+     * It generates a random number (no parameters are passed) and checks to see if it's already in the array of previously generated numbers, 
+     * and if it is, it generates another random number.
+     * While the finla line says it returns zero, it was likely never be used.
+     * 
+     * @return The return value is an integer.
+     */
     public static int generateUniqueRandomNumber() {
           //this bit for if this is the first run
           Random r = new Random( System.currentTimeMillis() );
@@ -29,6 +45,13 @@ public class Account {
     }
 
 
+    /**
+     * This method returns a boolean, depending on whether the account id is in the system or not.
+     * True if the account ID exists, and false otherwise.
+     * 
+     * @param Id The account ID to be checked
+     * @return The method isAccountIdRecognised is returning a boolean value.
+     */
     public static boolean isAccountIdRecognised(int Id) {
       for (int i = 0 ; i < accountArrayList.size(); i++) {
         if (((accountArrayList.get(i)).getAccountId()) == Id) {
@@ -38,6 +61,14 @@ public class Account {
       return false;
     }
 
+    /**
+     * This function takes in a string and returns a boolean. It returns true if the string is equal to
+     * the handle of any account in the accountArrayList. It returns false if the string is not equal
+     * to the handle of any account in the accountArrayList
+     * 
+     * @param handle the handle of the account to be checked
+     * @return The method is returning a boolean value.
+     */
     public static boolean doesHandleExist(String handle) {
       for (int i = 0; i < accountArrayList.size(); i++){
         if (((accountArrayList.get(i)).getHandle()).equals(handle)){
@@ -48,6 +79,12 @@ public class Account {
     } 
 
 
+    /**
+     * It returns true if the string contains a space or is empty
+     * 
+     * @param handle The handle to check for white space or emptiness.
+     * @return The method is returning a boolean value.
+     */
     public static boolean doesItContainWhiteSpaceOrIsEmpty(String handle) {
       for (int i = 0; i < handle.length(); i++) {
         if ((handle.charAt(i)) == (' ')) {
@@ -61,6 +98,13 @@ public class Account {
     }
 
 
+    /**
+     * If the handle contains white space, is empty, or if the handle is longer than 30 characters,
+     * the method returns true. Otherwise, it returns false.
+     * 
+     * @param handle The handle to be checked
+     * @return The method is returning a boolean value.
+     */
     public static boolean isHandleInvalid(String handle) {
       if (doesItContainWhiteSpaceOrIsEmpty(handle) == true || handle.length() > 30) {
         return true;
@@ -70,28 +114,58 @@ public class Account {
 
 
     //Getter Methods
+    /**
+     * This is a getter method that returns the accountId of the account
+     * 
+     * @return The accountId
+     */
     public int getAccountId() {
       return accountId;
     }
 
+    /**
+     * This function returns the handle of the user
+     * 
+     * @return The handle of the user.
+     */
     public String getHandle() {
       return handle;
     }
 
+    /**
+     * This function returns the description of the object
+     * 
+     * @return The description of the item.
+     */
     public String getDescription() {
       return description;
     }
 
 
     //Setter Methods
+    /**
+     * This function sets the accountId to the newAccountId
+     * 
+     * @param newAccountId The new account ID to set.
+     */
     public void setAccountId(int newAccountId) {
       this.accountId = newAccountId;
     }
 
+    /**
+     * This function sets the handle of the user to the new handle
+     * 
+     * @param newHandle The new handle to set.
+     */
     public void setHandle(String newHandle) {
       this.handle = newHandle;
     }
 
+    /**
+     * This function sets the description of the object to the newDescription parameter
+     * 
+     * @param newDescription The new description of the item.
+     */
     public void setDescription(String newDescription) {
       this.description = newDescription;
     }
--- src/socialmedia/Post.java
+++ src/socialmedia/Post.java
@@ -15,6 +15,12 @@ public class Post{
 
 
 
+  /**
+   * This function takes a string and returns true if the string is empty or longer than 100 characters
+   * 
+   * @param message The message that the user is trying to post.
+   * @return A boolean value.
+   */
   public static boolean isPostInvalid(String message) {
     if (message == "") {
       return true;
@@ -26,6 +32,14 @@ public class Post{
   }
 
 
+  /**
+   * It loops through the array list of posts and checks if the post id of the post at the current
+   * index is equal to the id passed in as a parameter. If it is, it returns true. If it isn't, it
+   * returns false
+   * 
+   * @param id the id of the post
+   * @return A boolean value
+   */
   public static boolean doesPostIdExist(int id) {
     for (int i = 0; i < postArrayList.size(); i++) {
       if ((postArrayList.get(i)).getPostId() == id) {
@@ -36,6 +50,13 @@ public class Post{
   }
 
 
+  /**
+   * This function checks if a post is an endorsement by checking if the post is an instance of the
+   * Endorsement class
+   * 
+   * @param id the id of the post
+   * @return The method is returning a boolean value.
+   */
   public static boolean isAnEndorsement(int id) {
     for (int i = 0; i < postArrayList.size(); i++) {
       if (((postArrayList.get(i)).getPostId()) == id) {
@@ -58,47 +79,112 @@ public class Post{
 
 
   //Getter methods
+  /**
+   * This function returns the postId of the post
+   * 
+   * @return The postId
+   */
   public int getPostId(){
     return postId;
   }
+  /**
+   * This function returns the account handle of the user
+   * 
+   * @return The handle of the account.
+   */
   public String getAccountHandle(){
     return handle;
   }
+  /**
+   * // Java
+   * public String getBody(){
+   *     return body;
+   *   }
+   * 
+   * @return The body of the email.
+   */
   public String getBody(){
     return body;
   }
+  /**
+   * This function returns the number of comments on a post
+   * 
+   * @return The number of comments.
+   */
   public int getCommentNUmber(){
     return numberOfComments;
   }
+  /**
+   * This method returns the number of endorsements a post has
+   * 
+   * @return The number of endorsements.
+   */
   public int getEndorsementNumber(){
     return numberOfEndorsements;
   }
-    public int getOriginalPostId(){
+  /**
+   * This function returns the original post ID of the post (Posts being Endorsements and Comments).
+   * If Post id is zero, then it is an original post. 
+   * 
+   * @return The original post ID.
+   */
+  public int getOriginalPostId(){
     return originalPostID;
   }
 
   //Seter methods
+  /**
+   * This function sets the postId to the newPostId passed.
+   * 
+   * @param newPostId The new post ID to set.
+   */
   public void setPostId(int newPostId) {
     this.postId = newPostId;
   }
+  /**
+   * This function sets the handle of the user
+   * 
+   * @param handle The handle of the user to be followed.
+   */
   public void setHandle(String handle) {
     this.handle = handle;
   }
+  /**
+   * The function takes in an integer, increments it by one, and then sets the value of the variable
+   * numberOfEndorsements to the new value
+   * 
+   * @param endorsementNumber The number of endorsements the user has.
+   */
   public void setEndorsements(int endorsementNumber){
     numberOfEndorsements =  ++endorsementNumber; 
   }
+  /**
+   * This function takes an integer as a parameter and sets the numberOfComments variable to the value
+   * of the parameter plus one
+   * 
+   * @param commentNumber The number of comments that the post has.
+   */
   public void setNumberOfComments(int commentNumber){
     numberOfComments = ++commentNumber;
   }
-    public void setBody(String body) {
+    
+  /**
+   * This function sets the body of the post
+   * 
+   * @param body The body of the message.
+   */
+  public void setBody(String body) {
     this.body = body;
   }
 
 
   //Constructor
+  /** Default constructor for post object
+   */
   public Post(){
     
   }
+  // This constructor creates a post object
   public Post(String handle,String body){
     this.body = body;
     this.handle = handle;
@@ -111,10 +197,16 @@ public class Post{
 }
 
 
+  /**
+   * The Endorsement class is a subclass of the Post class. It has a constructor that takes in a handle
+   * and an id. It also has a method called setEndorsementMessage that takes in a message and sets the
+   * endorsementMessage variable to that message
+   */
   class Endorsement extends Post{
 
   public String endorsementMessage;
 
+// This constructor creates an endorsement object
   public Endorsement(String handle,int id){
     super();
     this.handle = handle;
@@ -122,6 +214,11 @@ public class Post{
 
   }
 
+  /**
+   * This function sets the endorsement message to the message passed in as a parameter
+   * 
+   * @param message The endorsement message to be displayed on the endorsement.
+   */
   public void setEndorsementMessage(String message){
     endorsementMessage = message;
   }
